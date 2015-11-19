8ba13fa6765732036dc495e73ff2f0eecc3c5bf9
Shivam Srivastava <shivam01990@gmail.com>
2015-11-19 14:29:33 +0530

    App Alerts

--- COExtension/src/common/content.js
+++ COExtension/src/common/content.js
@@ -6,6 +6,8 @@
 // ==/UserScript==
 
 
+var $ = window.$.noConflict(true); // Required for IE
+
 function postPageInfoMessage() {
     var pageInfo = {
         url: document.URL
@@ -19,28 +21,52 @@ function postPageInfoMessage() {
 
 postPageInfoMessage();
 
-function GetAlertHTML()
-{
-	var rType = "";
-	rType +="<div style=\"position: fixed; width: 350px;right: 20px;top: 20px; z-index: 2147483647;background: rgb(255, 255, 255) !important;text-align: center; font-family: Arial, sans-serif;font-size: 14px; font-weight: bolder; color: rgb(73, 73, 73); line-height: 18px; box-sizing: content-box;  border-radius: 2px !important;\">";
-	rType +=" <div style=\" height: 60px;  background: rgb(3, 89, 66) !important; color: rgb(255, 255, 255); border-top-left-radius: 2px;  border-top-right-radius: 2px;\" class=\"tcb_boxhmin\">";
-	rType +="  <img src=\"http://in.collectoffers.com/img/collect-offers-logo.png\" height=\"50px\" class=\"tcb_logo\">";
-	rType +="  <span style=\" float: right; border: 1px solid rgb(255, 255, 255) !important;    border-image:  !important;  padding: 1px 7px !important; font-size: 11px; position: relative; top: 20px;  right: 10px;  cursor: pointer;\" class=\"tcb_close\">X</span>";
-	rType +="  <span style=\" float: right; border: 1px solid rgb(255, 255, 255) !important;    border-image:  !important;  padding: 1px 7px !important; font-size: 11px; position: relative; top: 20px;  right: 10px;  cursor: pointer;\" class=\"tcb_res\">-</span>";
-	rType +=" </div>";
-	rType +=" <div class=\"tcb_boxh\">";	
-	rType +="  <span class=\"tcb_min\">_</span>";
-	rType +=" </div>";
-	rType +=" <div style=\"border:  !important; border-image:  !important;  border-bottom-left-radius: 2px;  border-bottom-right-radius: 2px;\" class=\"tcb_boxw\">";
-	rType +="   <div class=\"tcb_boxc\">Visit collect offers to earn Up to 9% cashback on your purchases from this merchant.</div>";
-	rType +="   <a href=\"http://www.collectoffers.com\" style=\"  margin-bottom: 10px; padding: 10px !important;font-size: 12px; font-weight: 700; background: rgb(237, 28, 92) !important;  color: rgb(255, 255, 255);  text-decoration: none;  display: inline-block; vertical-align: middle; text-align: center; cursor: pointer; border-radius: 3px !important;\" class=\"tcb_boxbtn\" target=\"_blank\">Get Cashback Now</a>";
-	rType +=" </div>";
-	rType +="</div>";
-	return rType;
+function GetAlertHTML() {
+    var rType = "";
+    rType += "<div style=\"position: fixed; width: 350px;right: 20px;top: 20px; z-index: 2147483647;background: rgb(255, 255, 255) !important;text-align: center; font-family: Arial, sans-serif;font-size: 14px; font-weight: bolder; color: rgb(73, 73, 73); line-height: 18px; box-sizing: content-box;  border-radius: 2px !important;\">";
+    rType += " <div style=\" height: 60px;  background: rgb(3, 89, 66) !important; color: rgb(255, 255, 255); border-top-left-radius: 2px;  border-top-right-radius: 2px;\" class=\"tcb_boxhmin\">";
+    rType += "  <img src=\"http://in.collectoffers.com/img/collect-offers-logo.png\" height=\"50px\" class=\"tcb_logo\">";
+    rType += "  <span style=\" float: right; border: 1px solid rgb(255, 255, 255) !important;    border-image:  !important;  padding: 1px 7px !important; font-size: 11px; position: relative; top: 20px;  right: 10px;  cursor: pointer;\" class=\"tcb_close\">X</span>";
+    rType += "  <span style=\" float: right; border: 1px solid rgb(255, 255, 255) !important;    border-image:  !important;  padding: 1px 7px !important; font-size: 11px; position: relative; top: 20px;  right: 10px;  cursor: pointer;\" class=\"tcb_res\">-</span>";
+    rType += " </div>";
+    rType += " <div class=\"tcb_boxh\">";   
+    rType += " </div>";
+    rType += " <div style=\"border:  !important; border-image:  !important;  border-bottom-left-radius: 2px;  border-bottom-right-radius: 2px;\" class=\"tcb_boxw\">";
+    rType += "   <div class=\"tcb_boxc\">Visit collect offers to earn Up to 9% cashback on your purchases from this merchant.</div>";
+    rType += "   <a href=\"http://www.collectoffers.com\" style=\"  margin-bottom: 10px; padding: 10px !important;font-size: 12px; font-weight: 700; background: rgb(237, 28, 92) !important;  color: rgb(255, 255, 255);  text-decoration: none;  display: inline-block; vertical-align: middle; text-align: center; cursor: pointer; border-radius: 3px !important;\" class=\"tcb_boxbtn\" target=\"_blank\">Get Cashback Now</a>";
+    rType += " </div>";
+    rType += "</div>";
+    return rType;
 }
 
+//function GenerateGoogleAlert()
+//{
+//     var rType = "";
+//     rType+="<div style=\"position: fixed; width: 100%;z-index: 2147483647;background: rgb(3, 89, 66) !important;height: 26px;padding-top: 5px;text-align: center; font-family: Arial, sans-serif;font-size: 13px;font-weight: bolder;color: rgb(255, 255, 255); line-height: 18px;box-sizing: content-box;\" id=\"tcbnotify\">";
+//     rType+=" <span style=\"position: absolute;top: 0px;left: 5px;\"><img src=\"http://in.collectoffers.com/img/collect-offers-logo.png\" height=\"50px\" class=\"tcb_logo\"></span>";
+//     rType+=" <span id=\"tcbnotifycontent\" style=\"top: 7px;display: inline-block;position: absolute;left: 35px;right: 35px; text-align: center;white-space: nowrap; overflow: hidden !important;text-overflow: ellipsis;\">";
+//     rType+=" <a href=\"http://www.collectoffers.in/\" target=\"_blank\">FlipKart - Up to 9% cashback</a>";
+//     rType+=" </span>";
+//     rType+=" <span style=\"float: right; border: 1px solid rgb(255, 255, 255) !important; border-image:  !important;padding: 1px 7px !important;font-size: 11px; position: relative; bottom: 1px; right: 5px;cursor: pointer;\" class=\"tcbclose\">X </span>";
+//     rType+="</div>";
+//     return rType;
+//}
+
+var IsAlertDisplay = true;
 // handle messages from background script
-kango.addMessageListener('ShowDiscountAlert', function(event) {  
-   //alert('append to body'); 
-   document.body.innerHTML += GetAlertHTML();
+kango.addMessageListener('ShowDiscountAlert', function (event) {
+    if (IsAlertDisplay) {
+        $('body').append(GetAlertHTML());
+        displayalert = false;
+    }
 });
+
+//kango.addMessageListener('ShowGoogleDiscountAlert', function (event) {
+//    if (IsAlertDisplay) {
+//        $('body').append(GenerateGoogleAlert());
+//        displayalert = false;
+//    }
+//});
+
+
+
--- COExtension/src/common/main.js
+++ COExtension/src/common/main.js
@@ -7,34 +7,63 @@ function getDomainFromUrl(url) {
     return ((matches != null && typeof matches[1] != 'undefined') ? matches[1] : null);
 }
 
+var getQueryString = function (field, url) {
+    var href = url;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+};
+
 function COExtension() {
     var self = this;
-    kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function() {
+    kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function () {
         self._onCommand();
-    });   
+    });
 }
 
 
-kango.addMessageListener('PageInfo', function(event) {
+kango.addMessageListener('PageInfo', function (event) {
     // is message from active tab?
-      kango.console.log('Page info received\nTarget tab url=' + event.target.getUrl());
-          if (event.target.isActive()) {
-       // kango.console.log('Page info received\nTarget tab url=' + event.target.getUrl());
-        //kango.ui.browserButton.setTooltipText(getTooltipTextFromPageInfo(event.data));
-         var domain = getDomainFromUrl(getURLFromPageInfo(event.data));
-       // i don't like bing, redirect to google
+    kango.console.log('Page info received\nTarget tab url=' + event.target.getUrl());
+    if (event.target.isActive()) {
+        var domain = getDomainFromUrl(getURLFromPageInfo(event.data));
+        // i don't like bing, redirect to google
         if (domain == 'www.flipkart.com' || domain == 'flipkart.com') {
-             kango.console.log('domain is: '+domain);
-             event.target.dispatchMessage('ShowDiscountAlert');
-          }
+            kango.console.log('domain is: ' + domain);
+            event.target.dispatchMessage('ShowDiscountAlert');
         }
+    }
 });
 
+
+
 COExtension.prototype = {
-    _onCommand: function() {
-        kango.browser.tabs.create({url: 'http://www.collectoffers.com'});
+    _onCommand: function () {
+        kango.browser.tabs.create({ url: 'http://www.collectoffers.com' });
     }
 };
 
 
 var extension = new COExtension();
+
+//var _GoogleSearch = setInterval(function () { CheckGooglePage() }, 4000);
+
+//function CheckGooglePage() {
+//    kango.browser.tabs.getCurrent(function (tab) {
+//        // tab is KangoBrowserTab object
+//        var tempurl = tab.getUrl();
+//        kango.console.log(tempurl);
+//        var domain = getDomainFromUrl(tempurl);
+//        if (domain == 'www.google.co.in' || domain == 'google.co.in' || domain == 'www.google.com' || domain == 'google.com') {
+//            kango.console.log('domain is: ' + domain);
+//            var searchtext = getQueryString('q', tempurl);
+//            if (searchtext != null) {
+//                kango.console.log('searchtext is:' + searchtext);
+//                //var domaintext=domain.replace('www.','').replace('.com','');             
+//                if (searchtext.indexOf('flipkart') >= 0) {
+//                    tab.dispatchMessage('ShowGoogleDiscountAlert');
+//                }
+//            }
+//        }
+//    });
+//}
